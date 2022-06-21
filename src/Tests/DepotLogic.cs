696ff6e134b40c7c5607f04bd11c118e25b798ea
Martin Bryant <mail@martinbryant.dev>
2022-06-21 21:55:07 +0100

    Add mocking

--- src/Tests/DepotLogic.cs
+++ src/Tests/DepotLogic.cs
@@ -1,4 +1,5 @@
 using ef_core_example.Logic;
+using ef_core_example.Models;
 using GoldMarketplace.ServerAPIService.Repositories;
 using Moq;
 using Moq.AutoMock;
@@ -14,15 +15,116 @@ namespace ef_core_example.Tests
             _mock = new AutoMocker();
         }
         
+        [Fact(DisplayName = "[DepotLogic] - Create depot was successful")]
         public async Task CreateSuccess()
         {
             //Arrange
-            _mock.GetMock<IUnitOfWork>()
-                    .Setup(unit => unit.Commit());
+            var mockProfile = _mock.GetMock<IProfileLogic>();
+            var mockUnit    = _mock.GetMock<IUnitOfWork>();
+
+            mockProfile.Setup(profile => profile.GetProfile(It.IsAny<string>()))
+                        .ReturnsAsync(Profile.Create("valid"));
+
+            mockUnit.Setup(unit => unit.Commit());
+            mockUnit.Setup(unit => unit.Depots.Add(It.IsAny<Depot>()));
             
             var logic = _mock.CreateInstance<DepotLogic>();
-            
-            // var result = await logic.CreateDepot();
+
+            var depotDto = ValidDepotDto;
+
+            //Act
+            var result = await logic.CreateDepot(depotDto);
+
+            //Assert
+            Assert.True(result.IsSuccess);
+            mockProfile.Verify(profile => profile.GetProfile(It.IsAny<string>()),
+                Times.Once);
+            mockUnit.Verify(unit => unit.Depots.Add(It.IsAny<Depot>()), 
+                Times.Once);
+            mockUnit.Verify(unit => unit.Commit(), 
+                Times.Once);
+
         }
+
+        [Fact(DisplayName = "[DepotLogic] - Create depot profile not found")]
+        public async Task CreateDepotProfileNotFound()
+        {
+            //Arrange
+            var mockProfile = _mock.GetMock<IProfileLogic>();
+            var mockUnit    = _mock.GetMock<IUnitOfWork>();
+
+            mockProfile.Setup(profile => profile.GetProfile(It.IsAny<string>()))
+                        .ReturnsAsync(Errors.General.NotFound("Profile", Guid.NewGuid()));
+
+            mockUnit.Setup(unit => unit.Commit());
+            mockUnit.Setup(unit => unit.Depots.Add(It.IsAny<Depot>()));
+            
+            var logic = _mock.CreateInstance<DepotLogic>();
+
+            var depotDto = ValidDepotDto;
+
+            //Act
+            var result = await logic.CreateDepot(depotDto);
+
+            //Assert
+            Assert.Equal(Errors.General.Record_Not_Found, result.Error.Code);
+            mockProfile.Verify(profile => profile.GetProfile(It.IsAny<string>()),
+                Times.Once);
+            mockUnit.Verify(unit => unit.Depots.Add(It.IsAny<Depot>()), 
+                Times.Never);
+            mockUnit.Verify(unit => unit.Commit(), 
+                Times.Never);
+
+        }
+
+        [Fact(DisplayName = "[DepotLogic] - Create depot invalid depot info")]
+        public async Task CreateDepotInvalidDepotInfo()
+        {
+            //Arrange
+            var mockProfile = _mock.GetMock<IProfileLogic>();
+            var mockUnit    = _mock.GetMock<IUnitOfWork>();
+
+            mockProfile.Setup(profile => profile.GetProfile(It.IsAny<string>()))
+                        .ReturnsAsync(Profile.Create("valid"));
+
+            mockUnit.Setup(unit => unit.Commit());
+            mockUnit.Setup(unit => unit.Depots.Add(It.IsAny<Depot>()));
+            
+            var logic = _mock.CreateInstance<DepotLogic>();
+
+            var depotDto = ValidDepotDto;
+            depotDto.ContactEmail = null;
+
+            //Act
+            var result = await logic.CreateDepot(depotDto);
+
+            //Assert
+            Assert.Equal(Errors.General.Value_Is_Required, result.Error.Code);
+            mockProfile.Verify(profile => profile.GetProfile(It.IsAny<string>()),
+                Times.Once);
+            mockUnit.Verify(unit => unit.Depots.Add(It.IsAny<Depot>()), 
+                Times.Never);
+            mockUnit.Verify(unit => unit.Commit(), 
+                Times.Never);
+
+        }
+
+        public AddressDto ValidAddress => 
+            new AddressDto()
+            {
+                Address1 = "valid",
+                Address2 = "valid",
+                PostCode = "valid"
+            };
+
+        public DepotDto ValidDepotDto =>
+            new DepotDto()
+            {
+                DisplayName = "Name",
+                ContactName = "Name",
+                DeliveryAddress = ValidAddress,
+                BillingAddress = ValidAddress,
+                ContactEmail = "mail@mail.net"
+            };
     }
 }
